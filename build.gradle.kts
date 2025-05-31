d78148eca2fef53a40e16b3b4a6770e22f1e64fd
JohanVonElectrum <joanestradium@gmail.com>
2025-06-01 00:08:37 +0200

    Squashed commit of the following:
    commit 302d69eaaad4d6ba89a0f549f3d2c4b8396cbc40
    Author: JohanVonElectrum <joanestradium@gmail.com>
    Date:   Sun Jun 1 00:07:35 2025 +0200
    
        feat: fix stonecutter setup
    
    commit ae507c0fbb7b324670bf755ec3202f1126a9f04f
    Author: JohanVonElectrum <joanestradium@gmail.com>
    Date:   Sat May 31 07:55:28 2025 +0200
    
        feat: use stonecutter branches
    
        Not bundling due to `core` and `base` reading the same `mod.id`

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,84 @@
+import net.fabricmc.loom.task.RemapJarTask
+
 plugins {
-    id("fabric-loom") version "1.10-SNAPSHOT" apply false
+    id("java")
+    id("fabric-loom")
+}
+
+version = modInfo.version
+
+val minecraft = stonecutter.current.project
+
+val core: Project = stonecutter.node.sibling("ofa-core")!!.project
+val base: Project = stonecutter.node.sibling("ofa-base")!!.project
+
+val projects = arrayOf(core, base)
+
+base {
+    archivesName.set("${modInfo.id}-${stonecutter.current.project}")
+}
+
+stonecutter {
+    swap("mod.id", "\"${modInfo.prop("id")}\";")
+    swap("mod.name", "\"${modInfo.prop("name")}\";")
+    swap("mod.description", "\"${modInfo.prop("description")}\";")
+    swap("mod.version", "\"${modInfo.prop("version")}\";")
+}
+
+projects.forEach {
+    project.evaluationDependsOn(it.path)
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:$minecraft")
+    mappings("net.fabricmc:yarn:$minecraft+build.${modInfo.dep("fabric.yarn")}:v2")
+    modImplementation("net.fabricmc:fabric-loader:${modInfo.dep("fabric.loader")}")
+    modImplementation("net.fabricmc.fabric-api:fabric-api:${modInfo.dep("fabric.api")}+$minecraft")
+
+    projects.forEach {
+        implementation(project(it.path, configuration = "namedElements"))
+        implementation(it.sourceSets["client"].output)
+    }
+}
+
+loom {
+    runConfigs.all {
+        ideConfigGenerated(true)
+        runDir = "../run"
+    }
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    val java = if (stonecutter.eval(stonecutter.current.version, ">=1.20.5"))
+        JavaVersion.VERSION_21
+    else
+        JavaVersion.VERSION_17
+    sourceCompatibility = java
+    targetCompatibility = java
+}
+
+tasks.named<RemapJarTask>("remapJar") {
+    projects.forEach {
+        val remapTask = it.tasks.getByName<RemapJarTask>("remapJar")
+        dependsOn(remapTask)
+        inputs.files(remapTask.archiveFile)
+        nestedJars.from(remapTask.archiveFile)
+    }
+    addNestedDependencies.set(true)
+}
+
+tasks.processResources {
+    properties(listOf("fabric.mod.json"),
+        "mod.id" to modInfo.id,
+        "mod.name" to modInfo.name,
+        "mod.description" to modInfo.description,
+        "mod.version" to modInfo.version,
+        "deps.fabric.yarn" to modInfo.dep("fabric.yarn"),
+        "deps.fabric.loader" to modInfo.dep("fabric.loader"),
+        "deps.fabric.api" to modInfo.dep("fabric.api"),
+        "deps.minecraft" to minecraft,
+        "deps.java" to java.targetCompatibility.majorVersion
+    )
 }
---
+++ stonecutter.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("dev.kikugie.stonecutter")
+    id("fabric-loom") version "1.10-SNAPSHOT" apply false
+}
+stonecutter active "1.21.5"
+
+stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) { 
+    group = "project"
+    ofTask("build")
+}
+
+stonecutter registerChiseled tasks.register("chiseledClean", stonecutter.chiseled) {
+    group = "project"
+    ofTask("clean")
+}
+
+allprojects {
+    repositories {
+        mavenLocal()
+    }
+}
